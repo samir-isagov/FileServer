a6c4f5c8faac3f63ef7735cfb7bfb97668baaae1
Samir Isagov <samir.isagov@hotmail.com>
2020-07-26 19:00:32 +0400

    User button was able to be rectangle from right side when clicked. Keep round it. Add condition to plus new and search button - if user not logged in it will not be displayed

--- FileServer-AngularUI/src/app/app.component.ts
+++ FileServer-AngularUI/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {AuthService} from './_services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FileServer-AngularUI';
+  registerMode = false;
+
+  constructor(private authService: AuthService) {
+  }
+
+  loggedIn() {
+    return this.authService.loggedIn();
+  }
 }
